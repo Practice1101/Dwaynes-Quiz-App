210e1428b6e36f40a98c641fc74dfa84156f7536
Michael Blalock <mblalock90@yahoo.com>
2020-04-24 18:28:15 -0400

    added more functionality

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 'use strict';
-
-/**
- * Example store structure
- */
 const store = {
     // 5 or more questions are required
     questions: [
@@ -27,31 +23,38 @@ const store = {
             correctAnswer: '2019'
         }
     ],
-
-
-
 };
-let quizStarted = true;
-let questionNumber = 3;
+let quizStarted = false;
+let questionNumber = 0;
 let score = 0;
 
 function startScreen() {
-    let html = '<section class=\'start-section\'> <h2>Do you know the fundamentals of web design?</h2> <button class=\'start - button\' type="button"> <span>Start</span> </button> </section>';
+    let html = `<h2>Do you know the fundamentals of web design?</h2> 
+                <form class = 'startButton'>
+                    <button type="submit">Start</button>
+                </form>`;
+
     return html;
 }
 function quizScreen() {
+    let pulledQuestion = store.questions[questionNumber].question;
+    let answer1 = store.questions[questionNumber].answers[0];
+    let answer2 = store.questions[questionNumber].answers[1];
+    let answer3 = store.questions[questionNumber].answers[2];
+    let answer4 = store.questions[questionNumber].answers[3];
     let html = `<section class='question-section'>
                 <div class='question-answer-container'> 
                 <!--question container--> <div class='question-container'> 
-                <h3 class='question'>Question</h3> </div>
+                <h3 class='question'>${pulledQuestion}</h3> </div>
 
                 <!--Answers container-->
-                <form class='answers-container'>
-                    <input class='answer' type="radio" name='answer' value='0' required>Answer1<br />
-                    <input class='answer' type="radio" name='answer' value='1' required>Answer2<br />
-                    <input class='answer' type="radio" name='answer' value='2' required>Answer3<br />
-                    <input class='answer' type="radio" name='answer' value='3' required>Answer4<br />
-                </form>
+                <form class='answers-container' >
+                    <input class='answer' type="radio" name='answer' value='0' required>${answer1}<br />
+                    <input class='answer' type="radio" name='answer' value='1' required>${answer2}<br />
+                    <input class='answer' type="radio" name='answer' value='2' required>${answer3}<br />
+                    <input class='answer' type="radio" name='answer' value='3' required>${answer4}<br />
+                    <input type="submit" value = "Next Question">
+                    </form>
                 </div>
                 </section>`;
     return html;
@@ -62,23 +65,28 @@ function endScreen() {
                 </div>`
     return html;
 }
-
 function render() {
     let Str = '';
-    if (quizStarted === false) {
+    if(quizStarted === false) {
         Str = startScreen();
         $('main').html(Str);
     }
-
-    if (quizStarted === true && questionNumber > store.questions.length) {
+    if(quizStarted === true && questionNumber > store.questions.length) {
         Str = endScreen;
-        debugger;
         $('main').html(Str);
     }
-    else{
+    if(quizStarted === true && questionNumber < store.questions.length){
         Str = quizScreen();
         $('main').html(Str);
     }
 }
 
+
+
 render();
+
+$('.startButton').submit(function(event) {
+    quizStarted = true;
+    console.log(store.questions[questionNumber].answers[0]);
+    render();
+});
